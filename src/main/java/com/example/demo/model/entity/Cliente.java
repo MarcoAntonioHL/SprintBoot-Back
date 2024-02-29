2df824e7b76b22bdf6a6cb20c8edff0ad37390e8
Marco <marco.hl9antonio@gmail.com>
2024-02-28 21:58:02 -0500

    primer commit

---
+++ src/main/java/com/example/demo/model/entity/Cliente.java
@@ -0,0 +1,11 @@
+package com.example.demo.model.entity;
+
+import java.util.Date;
+
+public class Cliente {
+    private Integer idCliente;
+    private String nombre;
+    private String apellidos;
+    private String correo;
+    private Date fechaRegistro;
+}
